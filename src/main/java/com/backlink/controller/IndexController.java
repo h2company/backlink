4f1040b29a06e0562588439f365fd5fce5c9032d
QuoxAnk <quoxank1796>
2019-07-30 15:11:39 +0700

    AccountInfo ...

--- src/main/java/com/backlink/controller/IndexController.java
+++ src/main/java/com/backlink/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.backlink.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.backlink.entities.Account;
+import com.backlink.entities.AccountInfo;
+import com.backlink.services.AccountInfoService;
 import com.backlink.services.AccountService;
 
 @Controller
@@ -17,6 +21,9 @@ public class IndexController {
 	
 	@Autowired
 	private AccountService accountService;
+	
+	@Autowired
+	private AccountInfoService accountInfoService;
 	
 	@GetMapping(value = { "", "index" })
 	public String viewIndex() {
@@ -34,8 +41,13 @@ public class IndexController {
 	}
 
 	@PostMapping(value = "/register", produces = "application/json; charset=utf-8")
-	public @ResponseBody String actionRegistrator(HttpSession session, @RequestParam("username") String username) {
-		return accountService.save(new Account(username, "123", 1, "qa1796@gmail.com", "0377312606")).getHash();
+	public @ResponseBody String actionRegistrator(HttpSession session, @RequestParam("username") String username,
+			 @RequestParam("fullname") String fullname) {
+		String result = "Thêm " + username + " thất bại";
+		if(accountInfoService.save(new AccountInfo(username, fullname, "Nhà Giàu", new Date(), false)) != null) {
+			result = "Thêm " + username + " thành công";
+		}		
+		return result;
 	}
 
 	@GetMapping(value = "forgotpassword")
--- src/main/java/com/backlink/services/ServiceObject.java
+++ src/main/java/com/backlink/services/ServiceObject.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ServiceObject<T> {
-	List<T> findAll();
+public interface ServiceObject<T, V> {
+	<T> List<T> findAll();
 
-	T findById(String id);
+	T findById(V id);
 
 	T save(T t);
 
 	T update(T t);
 
-	boolean delete(T t);
+	void delete(V t);
 
 }
