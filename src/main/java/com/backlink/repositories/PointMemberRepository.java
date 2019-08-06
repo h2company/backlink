72efa0ae59e245b2f4dc0b6c42a12c3aa8790380
tuan <quoxank1796@gmail.com>
2019-08-06 11:46:17 +0700

    update

---
+++ src/main/java/com/backlink/repositories/PointMemberRepository.java
@@ -0,0 +1,13 @@
+package com.backlink.DTO;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.backlink.entities.PointMember;
+
+@Repository
+@Qualifier("pointMemberRepository")
+public interface PointMemberRepository extends JpaRepository<PointMember, Integer> {
+	
+}
