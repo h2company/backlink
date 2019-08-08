76e53e36ab3e106d7bfbfad64ba1443cf07190b0
QuoxAnk <quoxank1796>
2019-08-08 14:05:29 +0700

    recoverpassword

---
+++ src/main/java/com/backlink/repositories/RecoveryPasswordRepository.java
@@ -0,0 +1,13 @@
+package com.backlink.DTO;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.backlink.entities.RecoveryPassword;
+
+@Repository
+@Qualifier("recoverpassword")
+public interface RecoveryPasswordRepository extends JpaRepository<RecoveryPassword, Integer> {
+
+}
